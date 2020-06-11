df2f36479d45e1cd96caab1b1108c9c3e0388c49
taishi-i <taishi.ikeda.0323@gmail.com>
2020-06-11 21:20:25 +0900

    update v0.2.6

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Taishi Ikeda'
 author = 'Taishi Ikeda'
 
 # The short X.Y version
-version = '0.2.5'
+version = '0.2.6'
 # The full version, including alpha/beta/rc tags
-release = '0.2.5'
+release = '0.2.6'
 
 
 # -- General configuration ---------------------------------------------------
