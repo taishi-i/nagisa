e33d62ef8f5b1a6073eef31aca57a9bc3969ed89
taishi-i <taishi.ikeda.0323@gmail.com>
2026-06-29 00:36:25 +0900

    fix invalid escape sequence warning in tagger.py by using raw strings

--- nagisa/tagger.py
+++ nagisa/tagger.py
@@ -42,7 +42,7 @@ class Tagger(object):
         self.pattern = None
         if single_word_list:
             single_word_list = [utils.preprocess(w) for w in single_word_list if len(w) > 1]
-            single_word_list = [w.replace('(', '\(').replace(')', '\)')
+            single_word_list = [w.replace('(', r'\(').replace(')', r'\)')
                                 for w in single_word_list]
             single_word_list = sorted(single_word_list, key=lambda x:-len(x))
             if len(single_word_list) > 0:
